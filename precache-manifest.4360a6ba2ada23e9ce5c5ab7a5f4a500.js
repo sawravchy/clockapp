ea12f04636da0a3ccd56dc9961cfdc0ba4a139ec
sawravchy <sawravador@gmail.com>
2019-11-07 19:52:19 +0600

    Updates

---
+++ precache-manifest.4360a6ba2ada23e9ce5c5ab7a5f4a500.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba6b8a3e3638ded05d3fbe1816e25f26",
+    "url": "/compassapp/index.html"
+  },
+  {
+    "revision": "23f1d7cebacad8431a62",
+    "url": "/compassapp/static/css/main.65a92c1f.chunk.css"
+  },
+  {
+    "revision": "a13a5c0b59f9cb75ab7e",
+    "url": "/compassapp/static/js/2.a8d811bf.chunk.js"
+  },
+  {
+    "revision": "23f1d7cebacad8431a62",
+    "url": "/compassapp/static/js/main.f07a1489.chunk.js"
+  },
+  {
+    "revision": "6056483a88c1ba6af5e2",
+    "url": "/compassapp/static/js/runtime-main.6db5e359.js"
+  }
+]);
